3f95c1e43878717a6a4b6f4826a1e897b864d70c
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-08-28 21:58:45 -0400

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,23 @@
 <template>
-  <HelloWorld />
+	<v-container class="fill-height">
+		<v-responsive class="align-center text-center fill-height">
+			<v-img height="300" src="@/assets/logo.svg" />
+
+			<div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+			<h1 class="text-h2 font-weight-bold">Vuetify</h1>
+
+			<div class="py-14" />
+
+			<v-row class="d-flex align-center justify-center">
+				<v-col cols="auto">
+					<v-btn color="primary" variant="elevated" prepend-icon="mdi-login" size="large">
+						Login
+					</v-btn>
+				</v-col>
+			</v-row>
+		</v-responsive>
+	</v-container>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<script setup></script>
