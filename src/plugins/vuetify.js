dd2ed9001b5748ab56a1b979eaf64ce69fadc166
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-08-29 08:32:40 -0400

    [All] Defaults

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,6 +13,17 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  defaults: {
+    VBtn: {
+      class: 'text-none'
+    },
+    VTextField: {
+      variant: 'outlined'
+    },
+    VSelect: {
+      variant: 'outlined'
+    }
+  },
   theme: {
     themes: {
       light: {
