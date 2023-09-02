f07f4bb66945af0ab724cd733852ed252149a7e0
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-09-02 12:27:00 -0400

    [Admin] Cards

---
+++ src/components/CardElem.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card :color="props.color" variant="outlined">
+        <v-card-item>
+            <div class="d-flex flex-column flex-sm-row align-sm-center justify-sm-space-between">
+                <v-icon :icon="props.icon" size="50" class="mr-md-4" />
+                <div class="d-flex flex-column text-left text-md-right">
+                    <h4 class="text-h6">{{ props.title }}</h4>
+                    <div class="text-h4 text-md-h3"> {{ props.value }}</div>
+                </div>
+            </div>
+        </v-card-item>
+        <v-card-actions v-if="action?.to?.name">
+            <v-btn :to="action?.to" :text="action?.text" append-icon="mdi-arrow-right" variant="plain"></v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup>
+
+const props = defineProps(['color', 'title', 'icon', 'value', 'action']);
+
+</script>
--- src/views/admin/HomeView.vue
+++ src/views/admin/HomeView.vue
@@ -1,7 +1,42 @@
 <template>
     <base-view page-title="Dashboard" :page-breadcrumbs="[]" :requests="[
         method_getDashContent
-    ]"></base-view>
+    ]">
+
+        <template #content>
+            <v-row justify="center">
+                <!-- card users -->
+                <v-col cols="12" sm="5" md="4">
+                    <card-elem color="primary" icon="mdi-account-group-outline" title="Usuários"
+                        :value="data.cards.users.total" :action="{
+                            to: { name: 'admin.home' },
+                            text: 'Ir para usuários'
+                        }" />
+                </v-col>
+                <!-- /card users -->
+
+                <!-- card roles -->
+                <v-col cols="12" sm="5" md="4">
+                    <card-elem color="secondary" icon="mdi-shield-account-variant-outline" title="Funções"
+                        :value="data.cards.roles.total" :action="{
+                            to: { name: 'admin.home' },
+                            text: 'Ir para funções'
+                        }" />
+                </v-col>
+                <!-- /card roles -->
+
+                <!-- card example -->
+                <v-col cols="12" sm="5" md="4">
+                    <card-elem color="info" icon="mdi-text-box-plus-outline" title="Card Example #1" value="101" :action="{
+                        to: { name: 'admin.home' },
+                        text: 'Action example #1'
+                    }" />
+                </v-col>
+                <!-- /card example -->
+            </v-row>
+        </template>
+
+    </base-view>
 </template>
 
 <script setup>
@@ -9,6 +44,7 @@
 import { req } from '@/plugins/axios';
 import BaseView from '../BaseView.vue';
 import { ref } from 'vue';
+import CardElem from '@/components/CardElem.vue';
 
 /**
  * 
