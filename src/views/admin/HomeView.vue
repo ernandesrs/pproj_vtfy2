f89a2e3d8826f58667193c5070a5fa2677fe0758
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-09-02 16:27:00 -0400

    [Admin] Using content elem component

--- src/views/admin/HomeView.vue
+++ src/views/admin/HomeView.vue
@@ -4,36 +4,41 @@
     ]">
 
         <template #content>
-            <v-row justify="center">
-                <!-- card users -->
-                <v-col cols="12" sm="5" md="4">
-                    <card-elem color="primary" icon="mdi-account-group-outline" title="Usuários"
-                        :value="data.cards.users.total" :action="{
-                            to: { name: 'admin.home' },
-                            text: 'Ir para usuários'
-                        }" />
-                </v-col>
-                <!-- /card users -->
+            <content-elem borderless>
+                <template #content>
+                    <v-row justify="center">
+                        <!-- card users -->
+                        <v-col cols="12" sm="5" md="4">
+                            <card-elem color="primary" icon="mdi-account-group-outline" title="Usuários"
+                                :value="data.cards.users.total" :action="{
+                                    to: { name: 'admin.home' },
+                                    text: 'Ir para usuários'
+                                }" />
+                        </v-col>
+                        <!-- /card users -->
 
-                <!-- card roles -->
-                <v-col cols="12" sm="5" md="4">
-                    <card-elem color="secondary" icon="mdi-shield-account-variant-outline" title="Funções"
-                        :value="data.cards.roles.total" :action="{
-                            to: { name: 'admin.home' },
-                            text: 'Ir para funções'
-                        }" />
-                </v-col>
-                <!-- /card roles -->
+                        <!-- card roles -->
+                        <v-col cols="12" sm="5" md="4">
+                            <card-elem color="secondary" icon="mdi-shield-account-variant-outline" title="Funções"
+                                :value="data.cards.roles.total" :action="{
+                                    to: { name: 'admin.home' },
+                                    text: 'Ir para funções'
+                                }" />
+                        </v-col>
+                        <!-- /card roles -->
 
-                <!-- card example -->
-                <v-col cols="12" sm="5" md="4">
-                    <card-elem color="info" icon="mdi-text-box-plus-outline" title="Card Example #1" value="101" :action="{
-                        to: { name: 'admin.home' },
-                        text: 'Action example #1'
-                    }" />
-                </v-col>
-                <!-- /card example -->
-            </v-row>
+                        <!-- card example -->
+                        <v-col cols="12" sm="5" md="4">
+                            <card-elem color="info" icon="mdi-text-box-plus-outline" title="Example #1" value="101"
+                                :action="{
+                                    to: { name: 'admin.home' },
+                                    text: 'Example #1'
+                                }" />
+                        </v-col>
+                        <!-- /card example -->
+                    </v-row>
+                </template>
+            </content-elem>
         </template>
 
     </base-view>
@@ -45,6 +50,7 @@ import { req } from '@/plugins/axios';
 import BaseView from '../BaseView.vue';
 import { ref } from 'vue';
 import CardElem from '@/components/CardElem.vue';
+import ContentElem from '@/components/ContentElem.vue';
 
 /**
  * 
