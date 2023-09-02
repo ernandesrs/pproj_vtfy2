497dbd3eda066a85a8cb1a578b792cc2d2b441c4
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-09-02 16:26:44 -0400

    [All] Content elem component icon

--- src/components/ContentElem.vue
+++ src/components/ContentElem.vue
@@ -2,7 +2,7 @@
     <v-sheet>
         <v-card :elevation="props.borderless ? 0 : 1" class="px-6 py-6" :border="!props.borderless">
             <v-card-title v-if="title" class="font-weight-normal">
-                {{ title }}
+                <v-icon v-if="icon" :icon="icon" class="mr-2" /> {{ title }}
             </v-card-title>
             <v-card-item>
                 <slot name="content" />
@@ -19,7 +19,11 @@ const props = defineProps({
         default: false
     },
     title: {
-        type: [String],
+        type: [String, null],
+        default: null
+    },
+    icon: {
+        type: [String, null],
         default: null
     }
 });
