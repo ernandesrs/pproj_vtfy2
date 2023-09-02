ccdfad9e003767c3899cfc660c681dde8f6cbb84
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-09-02 16:10:58 -0400

    [All] Componnet content container

---
+++ src/components/ContentElem.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-sheet>
+        <v-card :elevation="props.borderless ? 0 : 1" class="px-6 py-6" :border="!props.borderless">
+            <v-card-title class="font-weight-normal">
+                {{ title }}
+            </v-card-title>
+            <v-card-item>
+                <slot name="content" />
+            </v-card-item>
+        </v-card>
+    </v-sheet>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    borderless: {
+        type: [Boolean],
+        default: false
+    },
+    title: {
+        type: [String],
+        default: null
+    }
+});
+
+</script>
