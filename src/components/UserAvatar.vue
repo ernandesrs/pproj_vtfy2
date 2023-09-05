baa2d3dd405ccef1350d47b97a60cba8a100fff4
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-09-05 19:29:00 -0400

    [All] User avatar updates

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,17 +1,26 @@
 <template>
-    <div class="border rounded-circle user-avatar">
-        <template v-if="photo_url">
-            <v-img :src="photo_url" cover />
-        </template>
-        <template v-else>
-            <span class="text-h2 font-weight-medium">{{ (props.username ? props.username[0] : 'U').toUpperCase() }}</span>
-        </template>
-    </div>
+    <template v-if="photo_url">
+        <v-avatar :image="photo_url" :size="size" />
+    </template>
+    <template v-else>
+        <div class="c-avatar rounded-circle d-flex justify-center align-center" :style="{
+            'width': props.size + 'px',
+            'height': props.size + 'px'
+        }">
+            <span class="font-weight-medium" :style="{
+                'font-size': (props.size / 2.75) + 'px'
+            }">{{ (props.username ? props.username[0] : 'U').toUpperCase() }}</span>
+        </div>
+    </template>
 </template>
 
 <script setup>
 
 const props = defineProps({
+    size: {
+        type: [Number],
+        default: 200
+    },
     photo_url: {
         type: [String, null],
         default: null
@@ -24,31 +33,8 @@ const props = defineProps({
 
 </script>
 
-<style scoped>
-.user-avatar {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    max-width: 175px;
-    min-width: 175px;
-    min-height: 175px;
-    border-width: 5px !important;
-    overflow: hidden;
-}
-
-@media(min-width: 600px) {
-    .user-avatar {
-        max-width: 225px;
-        min-height: 225px;
-    }
-}
-
-@media(min-width: 800px) {
-    .user-avatar {
-        max-width: 250px;
-        min-height: 250px;
-    }
+<style>
+.c-avatar{
+    border: 2.5px solid #dedddd;
 }
 </style>
